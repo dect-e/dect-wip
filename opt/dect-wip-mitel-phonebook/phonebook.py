cc8b82996b62188695ca86c98d5b0064b1ffcca1
Valentin Schlegel <val@kabelsalat.ch>
2023-10-26 22:13:10 +0200

    use api

--- opt/dect-wip-mitel-phonebook/phonebook.py
+++ opt/dect-wip-mitel-phonebook/phonebook.py
@@ -1,10 +1,7 @@
 from bottle import route, run, request, template
+import requests
 
-entries = [
-    {"name": "val", "extension" : "123"},
-    {"name": "anna", "extension" : "2323"},
-    {"name": "leo", "extension" : "4242"},
-]
+dect_wip_ip = "127.0.0.1:8080"
 
 xsi_template = """<?xml version="1.0" encoding="UTF-8"?>
 <Personal xmlns="http://schema.broadsoft.com/xsi”>
@@ -16,14 +13,19 @@ xsi_template = """<?xml version="1.0" encoding="UTF-8"?>
 % end
 </Personal>"""
 
+
+
 @route('/com.broadsoft.xsi-actions/v2.0/user/<caller>/directories/personal')
 def return_phonebook(caller):
+
     search_string = request.query.name
     search_index = request.query.start
     search_results = request.query.results
+
+    entries = requests.get(f"http://{dect_wip_ip}/api/v1/phonebook").json()
 
     print(f"{caller} hat nach {search_string} gesucht, i={search_index}, n={search_results}")
 
     return template(xsi_template, entries=entries)
 
-run(host='0.0.0.0', port=8081, debug=True)
+run(host='0.0.0.0', port=8082, debug=True)
